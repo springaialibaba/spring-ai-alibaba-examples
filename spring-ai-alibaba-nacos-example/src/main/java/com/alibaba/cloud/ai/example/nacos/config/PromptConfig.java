2f978321e53319b7ffc830ed1b0b970809bea645
黄震 <476749506@qq.com>
2025-01-15 17:35:42 +0800

    feat: 增加license header & 文件以空行结尾 & 增加readme

--- spring-ai-alibaba-nacos-example/src/main/java/com/alibaba/cloud/ai/example/nacos/config/PromptConfig.java
+++ spring-ai-alibaba-nacos-example/src/main/java/com/alibaba/cloud/ai/example/nacos/config/PromptConfig.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2024 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.alibaba.cloud.ai.example.nacos.config;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
@@ -11,31 +27,14 @@ import org.springframework.cloud.context.config.annotation.RefreshScope;
 @RefreshScope
 @ConfigurationProperties(prefix = "prompt")
 public class PromptConfig {
-    private String welcome;
-    private String goodbye;
-    private String error;
+    private String joke;
 
-    public String getWelcome() {
-        return welcome;
+    public String getJoke() {
+        return joke;
     }
 
-    public void setWelcome(String welcome) {
-        this.welcome = welcome;
+    public void setJoke(String joke) {
+        this.joke = joke;
     }
 
-    public String getGoodbye() {
-        return goodbye;
-    }
-
-    public void setGoodbye(String goodbye) {
-        this.goodbye = goodbye;
-    }
-
-    public String getError() {
-        return error;
-    }
-
-    public void setError(String error) {
-        this.error = error;
-    }
 }
--- spring-ai-alibaba-nacos-example/src/main/java/com/alibaba/cloud/ai/example/nacos/controller/PromptController.java
+++ spring-ai-alibaba-nacos-example/src/main/java/com/alibaba/cloud/ai/example/nacos/controller/PromptController.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2024 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.alibaba.cloud.ai.example.nacos.controller;
 
 import com.alibaba.cloud.ai.example.nacos.config.PromptConfig;
@@ -19,9 +35,9 @@ public class PromptController {
     @Autowired
     private ChatClient chatClient;
 
-    @GetMapping("/welcome")
-    public String getWelcomePrompt() {
-        return chatClient.prompt(new Prompt(promptConfig.getWelcome())).call().content();
+    @GetMapping("/joke")
+    public String generateJoke() {
+        return chatClient.prompt(new Prompt(promptConfig.getJoke())).call().content();
     }
 
 }
