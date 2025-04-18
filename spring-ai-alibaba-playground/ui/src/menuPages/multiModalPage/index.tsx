78b4e655d04f05a5f9a740ae431a2a76b72ee88c
HY-love-sleep <583699747@qq.com>
2025-04-18 12:24:30 +0800

    merge

--- spring-ai-alibaba-playground/ui/src/menuPages/multiModalPage/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import BasePage from "../components/BasePage";
-
-const MultiModalPage: React.FC = () => {
-  return <BasePage title="多模态"></BasePage>;
-};
-
-export default MultiModalPage;
