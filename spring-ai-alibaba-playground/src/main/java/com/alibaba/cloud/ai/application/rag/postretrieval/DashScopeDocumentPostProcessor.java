7337b38d50a92aa92702ce600673a9cc3ba10e0c
shown.Ji <yuluo08290126@gmail.com>
2025-06-11 21:02:10 +0800

    feat: update playground rag web search
    Signed-off-by: shown.Ji <yuluo08290126@gmail.com>

---
+++ spring-ai-alibaba-playground/src/main/java/com/alibaba/cloud/ai/application/rag/postretrieval/DashScopeDocumentPostProcessor.java
@@ -0,0 +1,5 @@
+package com.alibaba.cloud.ai.application.rag.postretrieval;/**
+ * @author yuluo
+ * @author <a href="mailto:yuluo08290126@gmail.com">yuluo</a>
+ */public class DashScopeDocumentPostProcessor {
+}
